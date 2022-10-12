81ef1b128ce290023bff1593c8a61f3c3312b557
asd1495 <armando.salcedo95@gmail.com>
2022-10-12 16:23:25 -0500

    connected submit form to google sheets

--- src/main.js
+++ src/main.js
@@ -26,3 +26,20 @@ closeBtns.forEach((closeBtn) => {
         });
     });
 });
+
+//Contact Form
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+      })
+    });
+  });

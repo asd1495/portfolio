1d22302962da9e3e2dd694eb89ac2570ff874a98
asd1495 <armando.salcedo95@gmail.com>
2022-10-12 21:12:19 -0500

    added scroll to top button

--- src/main.js
+++ src/main.js
@@ -20,3 +20,15 @@ window.addEventListener("load", function() {
       })
     });
   });
+
+//Scroll to top
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function () {
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})

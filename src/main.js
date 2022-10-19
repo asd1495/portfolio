fac361f99a4a3d71d5079f76fdf869ade0ae0c60
asd1495 <armando.salcedo95@gmail.com>
2022-10-18 23:16:10 -0500

    fully implemented scrollreveal

--- src/main.js
+++ src/main.js
@@ -102,5 +102,19 @@ ScrollReveal({
     delay: 100
 });
 
-    //Target elements
-ScrollReveal().reveal('.home .info h2', { delay: 500, origin: 'left' });
+//Target elements 'Home'
+ScrollReveal().reveal('.home .info h2, .section-title-01, section-title-02', { delay: 500, origin: 'left' });
+
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 600, origin: 'right' });
+
+ScrollReveal().reveal('.home .info .btn', { delay: 700, origin: 'bottom' });
+
+ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200 });
+
+ScrollReveal().reveal('.home-img, .about-img', { delay: 500, origin: 'bottom' });
+
+ScrollReveal().reveal('.about .description, .copy-right', { delay: 600, origin: 'right' });
+
+ScrollReveal().reveal('.skills-description, .contact-left h2', { delay: 700, origin: 'left' });
+
+ScrollReveal().reveal('.education, .portfolio .img-card', { delay: 800, origin: 'bottom', interval: 200 });
